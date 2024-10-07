c90b98223e9f2f0e5f1cd19d0694d661502b7511
Bezaleel <142726056+Bezaleelderoys@users.noreply.github.com>
2024-10-07 09:10:21 +0800

    Add files via upload

---
+++ db/apotek_rpl5 (1).sql
@@ -0,0 +1,364 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 07, 2024 at 03:09 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `apotek_rpl5`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_detail_transaksi`
+--
+
+CREATE TABLE `tb_detail_transaksi` (
+  `iddetailtransaksi` int(4) NOT NULL,
+  `idtransaksi` int(3) NOT NULL,
+  `idobat` int(4) NOT NULL,
+  `jumlah` int(3) NOT NULL,
+  `hargasatuan` double NOT NULL,
+  `totalharga` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_detail_transaksi`
+--
+
+INSERT INTO `tb_detail_transaksi` (`iddetailtransaksi`, `idtransaksi`, `idobat`, `jumlah`, `hargasatuan`, `totalharga`) VALUES
+(3821, 1, 5, 1, 7000, 7000),
+(3822, 2, 3, 1, 5500, 5500),
+(3823, 3, 4, 1, 13000, 13000),
+(3824, 4, 2, 1, 3200, 3200),
+(3825, 5, 4, 1, 13000, 13000),
+(3826, 6, 5, 2, 7000, 14000),
+(3827, 7, 4, 2, 13000, 26000),
+(3828, 8, 5, 2, 7000, 14000),
+(3829, 9, 5, 2, 7000, 14000),
+(3830, 11, 5, 2, 7000, 14000),
+(3831, 14, 5, 3, 7000, 21000),
+(3832, 14, 2, 1, 3200, 3200),
+(3833, 14, 2, 1, 3200, 3200),
+(3834, 14, 2, 1, 3200, 3200),
+(3835, 14, 2, 1, 3200, 3200),
+(3836, 14, 2, 1, 3200, 3200),
+(3837, 14, 2, 1, 3200, 3200),
+(3838, 15, 5, 1, 7000, 7000),
+(3839, 16, 3, 1, 5500, 5500),
+(3840, 17, 3, 1, 5500, 5500);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_karyawan`
+--
+
+CREATE TABLE `tb_karyawan` (
+  `idkaryawan` int(4) NOT NULL,
+  `namakaryawan` varchar(50) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `telp` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_karyawan`
+--
+
+INSERT INTO `tb_karyawan` (`idkaryawan`, `namakaryawan`, `alamat`, `telp`) VALUES
+(1, 'Muhammad Iqbal', 'Jalan Pulau Enggano', '08997772882'),
+(2, 'Adi Kusuma', 'Jalan Pulau Saelus no 80', '0874910203'),
+(3, 'Fernando Simatupang', 'Jalan Nusa Kambangan No 99', '08857491923'),
+(4, 'Arya Faisal', 'Jalan Pulau Ayu No 28', '0883918274'),
+(5, 'Muhammad Sufyan', 'Jalan Teuku Umar No 100', '08977384912'),
+(6, 'Alif Rizki Raditya', 'Jalan Pulau Bungin No 200', '08749275239'),
+(7, 'Tugus Maha ', 'Jalan Tukad Citarum No 150', '089774928209');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_login`
+--
+
+CREATE TABLE `tb_login` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(65) NOT NULL,
+  `leveluser` varchar(50) NOT NULL,
+  `idkaryawan` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_login`
+--
+
+INSERT INTO `tb_login` (`username`, `password`, `leveluser`, `idkaryawan`) VALUES
+('2gus', 'admin', 'Admin', 7),
+('arya.faisal', 'admin123', 'Admin', 4),
+('fernandos', 'admin123', 'Admin', 3),
+('iqbal123', 'iqbal123', 'Admin', 1),
+('k.adi', 'adikusuma1', 'Admin', 2),
+('msufyan', 'aksdoe929a', 'Admin', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_obat`
+--
+
+CREATE TABLE `tb_obat` (
+  `id_obat` int(4) NOT NULL,
+  `id_supplier` int(4) NOT NULL,
+  `namaobat` varchar(100) NOT NULL,
+  `kategoriobat` varchar(50) NOT NULL,
+  `hargajual` double NOT NULL,
+  `hargabeli` double NOT NULL,
+  `stok_obat` int(11) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_obat`
+--
+
+INSERT INTO `tb_obat` (`id_obat`, `id_supplier`, `namaobat`, `kategoriobat`, `hargajual`, `hargabeli`, `stok_obat`, `keterangan`) VALUES
+(1, 1, 'Bodrex Extra 4 Kaplet', 'Kaplet', 2180, 2180, 33, 'Sakit kepala adalah rasa sakit atau nyeri di kepala, yang bisa muncul secara bertahap atau mendadak. Nyeri bisa muncul di salah satu sisi kepala, atau di seluruh bagian kepala. Sakit kepala bisa membuat kepala terasa berdenyut, atau seperti terlilit kencang oleh tali. Sakit kepala bisa terasa ringan hingga berat, dan dapat berlangsung beberapa jam hingga berhari-hari. Umumnya sakit kepala dapat diobati dengan obat-obatan antinyeri yang dijual bebas. Salah satu obat yang dapat Anda konsumsi adalah Bodrex.'),
+(2, 2, 'Mixagrip Strip Isi 4 Obat', 'Kaplet', 3200, 3200, 16, 'Indikasi Umum\r\nMenyembuhkan gejala flu seperti bersin-bersin, hidung berair, hidung tersumbat, demam, sakit kepala, dan nyeri otot.\r\n\r\nDeskripsi\r\nMIXAGRIP FLU adalah obat yang digunakan untuk meringankan gejala-gejala flu seperti demam, sakit kepala, hidung tersumbat dan bersin-bersin.\r\n\r\nKategori\r\nBatuk dan Flu\r\n\r\nKomposisi\r\nParacetamol 500 mg, Phenylephrine HCl 15 mg, Chlorpheniramin maleate 2 mg\r\n\r\nDosis\r\nDewasa : 3 - 4 kali per hari 1 - 2 tablet. Anak : 3 - 4 kali per hari 1/2 - 1 tablet\r\n\r\nAturan Pakai\r\nSesudah makan\r\n\r\nKemasan\r\nDus, 25 Catch Cover @ 1 Strip @ 4 kaplet'),
+(3, 3, 'Omeprazole 20 Mg', 'Pil', 5500, 5500, 121, 'Omeprazole obat apa? Omeprazole Novell merupakan obat generik dengan zat aktif Omeprazole yang digunakan untuk mengatasi penyakit-penyakit yang disebabkan oleh kelebihan produksi asam lambung, seperti sakit maag dan tukak lambung.\r\n\r\nMekanisme kerja dari Omeprazole yaitu: • Omeprazole termasuk golongan PPI (Proton Pump Inhibitor) yang efektif bekerja dengan menghambat sekresi asam lambung melalui sistem enzim adenosin trifosfatase hidrogen-kalium (pompa proton) dari sel parietal lambung. \r\n\r\nKomposisi\r\n\r\nOmeprazole\r\n\r\nKemasan\r\n\r\n1 Dos isi 3 Strip x 10 Tablet\r\n\r\nIndikasi / Manfaat / Kegunaan :\r\n\r\nTerapi jangka pendekulkus duodenal dan lambung. Refluks esofagitis, sindroma Zollinger-Ellison\r\n\r\nSub Kategori\r\n\r\nAntasid, Obat Antirefluks & Antiulserasi'),
+(4, 4, 'Panadol Paracetamol Obat', 'Kaplet', 13000, 13000, 20, 'supplier obat'),
+(5, 3, 'Dermacolin 10 Tablet', 'Tablet', 7000, 7000, 9, 'obat obatan'),
+(6, 4, 'Wawad', 'test', 12000, 15000, 22, 'Test');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pelanggan`
+--
+
+CREATE TABLE `tb_pelanggan` (
+  `idpelanggan` int(4) NOT NULL,
+  `namalengkap` varchar(50) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `telp` varchar(15) NOT NULL,
+  `usia` int(3) NOT NULL,
+  `buktifotoresep` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_pelanggan`
+--
+
+INSERT INTO `tb_pelanggan` (`idpelanggan`, `namalengkap`, `alamat`, `telp`, `usia`, `buktifotoresep`) VALUES
+(1, 'Bezaleel Elderoy Sulastiyo', 'Jalan Tukad Citarum', '08833919274', 17, '129230.jpeg'),
+(2, 'Aurelio Fransiskus Sinarta', 'Jalan Pulau Ayu No 27', '08771298334', 17, '93840.jpeg'),
+(3, 'Muhammad Jaffan Hanindito', 'Jalan Patih Jelantik', '08744219335', 18, '759819.jpeg'),
+(4, 'M. Hidayatullah', 'Jalan Nangka Selatan No 90', '08774912398', 20, '38520.jpeg'),
+(5, 'Henri Saputra', 'Jl Pulau Buton No 100', '089371274945', 18, 'icons8-valorant-96.png'),
+(14, 'Munir Said bin Thalib', 'Jalan Tukad Citarum', '08793845566', 33, 'heatsink.jpg'),
+(17, 'Sadewa Bharaka', 'Jalan Tukad Citarum', '08793845566', 33, '116-1164164_logo-router-packet-tracer-hd-png-download.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_supplier`
+--
+
+CREATE TABLE `tb_supplier` (
+  `id_supplier` int(4) NOT NULL,
+  `perusahaan` varchar(100) NOT NULL,
+  `telp` varchar(20) NOT NULL,
+  `alamat` text NOT NULL,
+  `keterangan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_supplier`
+--
+
+INSERT INTO `tb_supplier` (`id_supplier`, `perusahaan`, `telp`, `alamat`, `keterangan`) VALUES
+(1, 'PT Semua Obat', '08917738849', 'Jl Tukad Citarum No. 1 Denpasar Selatan', 'Supplier obat obatan'),
+(2, 'PT Kimia Farma', '087722388991', 'Jalan Tukad Citarum No 2 Denpasar Selatan', 'supplier obat sirup'),
+(3, 'PT Rajawali Obat', '087733992299', 'Jalan Tukad Citarum No 30 Denpasar', 'supplir obat pak rusdi\r\n'),
+(4, 'PT Fransiskus Abadi', '083199988774', 'Jalan Pulau Bungin No 24', 'supplier obat'),
+(5, 'PT Indobat', '08887722883', 'Jalan Raya Kuta no 151', 'obat obatan'),
+(6, 'PT Global Digital Verse', '08749212345', 'Jalan Pulau Ayu', 'Obat Herbal');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_transaksi`
+--
+
+CREATE TABLE `tb_transaksi` (
+  `idtransaksi` int(4) NOT NULL,
+  `idpelanggan` int(4) NOT NULL,
+  `idkaryawan` int(4) NOT NULL,
+  `tgltransaksi` date NOT NULL,
+  `kategoripelanggan` varchar(20) NOT NULL,
+  `totalbayar` double DEFAULT NULL,
+  `bayar` double DEFAULT NULL,
+  `kembali` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_transaksi`
+--
+
+INSERT INTO `tb_transaksi` (`idtransaksi`, `idpelanggan`, `idkaryawan`, `tgltransaksi`, `kategoripelanggan`, `totalbayar`, `bayar`, `kembali`) VALUES
+(1, 1, 1, '2024-08-10', 'Reguler', 7000, 10000, 3000),
+(2, 2, 2, '2024-08-09', 'Reguler', 5500, 6000, 500),
+(3, 3, 3, '2024-07-16', 'Reguler', 13000, 15000, 2000),
+(4, 4, 4, '2024-08-10', 'Reguler', 3200, 5000, 1800),
+(5, 5, 5, '2024-08-10', 'Reguler', 20000, 20000, 0),
+(6, 1, 3, '2024-10-07', 'Normal', NULL, NULL, NULL),
+(7, 3, 3, '2024-10-07', 'Normal', NULL, NULL, NULL),
+(8, 5, 3, '2024-10-07', 'Normal', NULL, NULL, NULL),
+(9, 2, 3, '2024-10-07', 'Normal', NULL, NULL, NULL),
+(10, 1, 3, '2024-10-07', 'Normal', NULL, NULL, NULL),
+(11, 17, 3, '2024-10-07', 'Normal', NULL, NULL, NULL),
+(12, 17, 3, '2024-10-07', 'Normal', NULL, NULL, NULL),
+(13, 2, 3, '2024-10-07', 'Normal', NULL, NULL, NULL),
+(14, 5, 3, '2024-10-07', 'Normal', NULL, NULL, NULL),
+(15, 4, 3, '2024-10-07', 'Normal', 7000, 10000, 3000),
+(16, 3, 3, '2024-10-07', 'Normal', 5500, 6000, 500),
+(17, 4, 3, '2024-10-07', 'Normal', 5500, 10000, 4500);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_detail_transaksi`
+--
+ALTER TABLE `tb_detail_transaksi`
+  ADD PRIMARY KEY (`iddetailtransaksi`),
+  ADD KEY `FK_idtransaksi` (`idtransaksi`),
+  ADD KEY `FK_idobat` (`idobat`);
+
+--
+-- Indexes for table `tb_karyawan`
+--
+ALTER TABLE `tb_karyawan`
+  ADD PRIMARY KEY (`idkaryawan`);
+
+--
+-- Indexes for table `tb_login`
+--
+ALTER TABLE `tb_login`
+  ADD PRIMARY KEY (`username`),
+  ADD KEY `idkaryawan` (`idkaryawan`);
+
+--
+-- Indexes for table `tb_obat`
+--
+ALTER TABLE `tb_obat`
+  ADD PRIMARY KEY (`id_obat`),
+  ADD KEY `supplier` (`id_supplier`);
+
+--
+-- Indexes for table `tb_pelanggan`
+--
+ALTER TABLE `tb_pelanggan`
+  ADD PRIMARY KEY (`idpelanggan`);
+
+--
+-- Indexes for table `tb_supplier`
+--
+ALTER TABLE `tb_supplier`
+  ADD PRIMARY KEY (`id_supplier`);
+
+--
+-- Indexes for table `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  ADD PRIMARY KEY (`idtransaksi`),
+  ADD KEY `FK_karyawan` (`idkaryawan`),
+  ADD KEY `FK_pelanggan` (`idpelanggan`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_detail_transaksi`
+--
+ALTER TABLE `tb_detail_transaksi`
+  MODIFY `iddetailtransaksi` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3841;
+
+--
+-- AUTO_INCREMENT for table `tb_karyawan`
+--
+ALTER TABLE `tb_karyawan`
+  MODIFY `idkaryawan` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tb_obat`
+--
+ALTER TABLE `tb_obat`
+  MODIFY `id_obat` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `tb_pelanggan`
+--
+ALTER TABLE `tb_pelanggan`
+  MODIFY `idpelanggan` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `tb_supplier`
+--
+ALTER TABLE `tb_supplier`
+  MODIFY `id_supplier` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  MODIFY `idtransaksi` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_detail_transaksi`
+--
+ALTER TABLE `tb_detail_transaksi`
+  ADD CONSTRAINT `FK_idobat` FOREIGN KEY (`idobat`) REFERENCES `tb_obat` (`id_obat`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_idtransaksi` FOREIGN KEY (`idtransaksi`) REFERENCES `tb_transaksi` (`idtransaksi`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tb_login`
+--
+ALTER TABLE `tb_login`
+  ADD CONSTRAINT `FK_login` FOREIGN KEY (`idkaryawan`) REFERENCES `tb_karyawan` (`idkaryawan`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tb_obat`
+--
+ALTER TABLE `tb_obat`
+  ADD CONSTRAINT `FK_supplier` FOREIGN KEY (`id_supplier`) REFERENCES `tb_supplier` (`id_supplier`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  ADD CONSTRAINT `FK_karyawan` FOREIGN KEY (`idkaryawan`) REFERENCES `tb_karyawan` (`idkaryawan`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_pelanggan` FOREIGN KEY (`idpelanggan`) REFERENCES `tb_pelanggan` (`idpelanggan`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
